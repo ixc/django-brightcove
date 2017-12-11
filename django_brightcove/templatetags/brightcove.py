a2cbfe20c1761543e9937e84f212bce96f299f0e
James Murty <james@murty.co>
2017-12-11 22:59:06 +1100

    Render Brightcove Player instead of legacy Smart Player
    Replace the use of Brightcove's legacy, and soon
    to be retired, "Smart Player" version with the
    newer "Brightcove Player" that will continue
    working after December 31 2017.
    
    Use default IFRAME embed technique by default.
    
    Use VIDEO HTML tag embed if `advanced` optional
    argument is provided with a truthy value.
    
    See https://support.brightcove.com/quick-start-brightcove-player#embedinpage

--- django_brightcove/templatetags/brightcove.py
+++ django_brightcove/templatetags/brightcove.py
@@ -7,32 +7,50 @@ register = template.Library()
 
 @register.inclusion_tag('tags/brightcove_player.html',
                         takes_context=True)
-def brightcove_player(context, player_id, *args, **kwargs):
+def brightcove_player(context, video_id, *args, **kwargs):
     """
-    This tags generate the brightcove object code. It required the
-    BRIGHTCOVE_PLAYER constant to be properly setup in
-    the django settings.
+    This tag generates the VIDEO HTML tag to render the Brightcove Player.
+    It requires the BRIGHTCOVE_PLAYER setting dict to be properly setup in
+    the django setting.
     """
     if not settings.BRIGHTCOVE_PLAYER:
-        raise Exception('BRIGHTCOVE_PLAYER constant is missing from your settings.')
+        raise Exception(
+            "BRIGHTCOVE_PLAYER setting is missing from your settings.")
     player = kwargs.get('player', 'default')
 
     try:
         player = settings.BRIGHTCOVE_PLAYER[player]
     except:
-        raise KeyError('%s player type is missing from the BRIGHTCOVE_PLAYER constant' % player)
+        raise KeyError(
+            "'%s' player type is missing from the BRIGHTCOVE_PLAYER setting"
+            % player)
 
+    # Get Brightcove Account ID from global BRIGHTCOVE_ACCOUNT_ID setting or
+    # player-specific ACCOUNT_ID setting.
     try:
-        context['playerID'] = player['PLAYERID']
-        context['playerKey'] = player['PLAYERKEY']
-    except:
-        raise KeyError('%s player type from the BRIGHTCOVE_PLAYER constant is not properly configured' % player)
+        context['account_id'] = settings.BRIGHTCOVE_ACCOUNT_ID
+    except AttributeError:
+        pass
+    try:
+        context['account_id'] = player['ACCOUNT_ID']
+    except KeyError:
+        pass
+    if 'account_id' not in context:
+        raise Exception(
+            "Brightcove Account ID must be set as global BRIGHTCOVE_ACCOUNT_ID"
+            " setting or as ACCOUNT_ID setting for the player '%s' in"
+            " the BRIGHTCOVE_PLAYER setting"
+            % player)
 
+    context['video_id'] = video_id
+    context['player_id'] = player.get('PLAYERID', 'default')
+    context['application_id'] = player.get('APPLICATION_ID', None)
+    context['embed_id'] = player.get('EMBED_ID', 'default')
+    # Use default IFrame embed or "advanced" in-page VIDEO embed?
+    context['use_advanced_embed'] = kwargs.get('advanced', False)
     context['width'] = kwargs.get('width', 480)
     context['height'] = kwargs.get('height', 270)
-    context['bgcolor'] = kwargs.get('bgcolor', '#FFFFFF')
-    context['isVid'] = kwargs.get('isVid', True)
-    context['isUI'] = kwargs.get('isUI', True)
-    context['dynamicStreaming'] = kwargs.get('dynamicStreaming', True)
-    context['player_id'] = player_id
+    if 'isUI' in kwargs:
+        context['show_controls'] = kwargs['isUI']  # Backwards-compatible
+    context['show_controls'] = kwargs.get('show_controls', True)
     return context
